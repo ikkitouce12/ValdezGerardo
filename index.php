58d7bd6d4f5906ac117f0dae0db0e6e4167fa29b
ikkitouce12 <93734124+ikkitouce12@users.noreply.github.com>
2022-10-30 23:37:40 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -12,21 +12,22 @@ include("marco-right.php");
    <div  style="width: 100%;">     
     <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
        <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
-        	<h1>¿Quién es Stalin Francis?</h1>
+        	<h1>¿Quién es Geardo Valdez?</h1>
        		 <hr>
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
-                Posteado por:  Stalin Francis<br>
-                Fecha: 2021-08-17 <br>
+                Posteado por:  Gerardo Valdez<br>
+                Fecha: 2022-30-10<br>
           </div>
         </div>
     </div>
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">Hola, yo soy Stalin Francis, ingeniero esmeraldeño que labora desde hace 10 años en la Universidad Tecnica Luis Vargas Torres, e impartido diferentes cátedras en la carrera de Tecnología de la Información entre las cuales esta Ingeniería de Software, Programación Web, Proyectos informáticos entre otras; pero la cátedra en la que mejor me desempeño es la de Fundamentos de programación que se dicta en el primer ciclo de la carrera de Tecnología de la Información. </p>
-                   <p class="eys-parrafo">Desde mís estudios secundarios me he apacionado por las ciencias y la tecnología, quieza sería  por esta capacidad que tiene estas dos áres del conocimiento de solucionar muchos probemas de la sociedad; es por eso que desde que termine mis estudios de ingeniería en la ESPOL, me he dedicado a la programación de computadoras. </p>
-<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el tercer ciclo de universidad fue el <b><a href="https://en.wikipedia.org/wiki/BASIC"> basic</a></b>  para este tiempo la ESPOL contaba con laboratorios de computadora  una computadora personal de marca IBM de tip XT modelo 5160  en es.</p>
+                   <p class="eys-parrafo"> Hola , yo soy Gerardo Valdez , estudiante de la carrera de Ing. en Tic`s` actuamente me encutro crusando el 7mo año de carrera en la Universidad Tecnica Luis Vargas Torres, en la cual me gusta mucho y me apasiona cada dia mas el tema de la tecnologia .</p>
+                   <p class="eys-parrafo"> Desde que estudie la secundaria me gusto mucho lo que era la informatica y la tecnologia , por la cual quise seguir esta carrera , hace pocos meses comense a incurcionar en lo que era el software libre y me encutro aprendiendo cada dia un poco mas sobre este nuevo
+				   mundo el cual es muy interezante y facinante.</p>
+<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar fue en el colegio el cual era c++ <b></b></p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -34,23 +35,22 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fisco-Misional Sagado Corazon donde me gradue en el año 1989, de la especialidad de Físico-Matemático, apenas me gradue viaje a Guayaquil para seguir mis estudios universitarios en la Escuela Superior Politécnica del Litoral donde seguí la carrera de Ingeniería en Computación, gracias a la demanda que exitía en este tiempo de este tipo de profesionales comencé a trabajar mientra estaba estudiando, prestando mis servicios profesionales en empresas como Poligráfica, El telegráfo, Intecorsorcio; mientras trabajaba en esta última logré obtener mi título de Ingeniero de Computación.</p>
-                   <p class="eys-parrafo">Ya con el título en la mano decidi regrasar a mi tierra Esmeraldas, con la finalidad de aplicar los conocimiento y habilidades obtenida por lo que forme la empresa EDUCAYSOFT, pero como los ingreso no fueron suficiente entre a trabajar al colegio Eloy Alfaro y posteiormente a la Universidad Técnica Luis Vargas Torres donde actualmente laboró.</p>
-<p class="eys-parrafo"> Por las exigencias de la universidad, ingresar a la maestria de docencia y desarrollo del curriculo, de la cual de grauduo obteniendo el título de Magister, pero despues realice otra maestría en la POLITECNICA, de Ciencias de la Computación.</p>
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Particula a Distancia Juan Montalvo donde me gradue en el año 2013, de la especialidad de Informatica, apenas me gradue me incline por mi otra pascion que fue la musica y me decide por irme a estudiar a la Universidad de las Artes ubicada en la ciudad de Guayaquil , pero retorne a la ciudad donde ya en el año 2018 retome mis estudios informaticos Ingresando a la Universidad Luis Vargas Torres en la carrera de Ing. en Tic`s`, donde cruzando el primer año comense a laborar como docente en el Colegio Particular Cristiano del Salvador diviendo el tiempo entre la Universidad y el trabajo donde acquire mucha esperiencia como docente implantiendo 
+				   materias como Dibujo tecnico y artisco, Musica, Ciencias Sociales ,Cultura Fisca y en ocaciones una que otra catedra de programacion a los estudiantes de primero de bachillerato.</p>
+				   <p class="eys-parrafo"></p>
+<p class="eys-parrafo"> </p>
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
     
 
-
-                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, mi primer programa lo hice en el leguaje Basic, utilizando el computador de la espol, luego programe c++, donde realiza algunos programa que leina el entregas del puerto serial y enviaban señales a un motor. Profesionalmente comenza programando en Informix un lenguaje de programación para el sistema Unix, utilizando uan versio de foxpro para unix, luego cuando aparecio la interface gráfica programe en foxpro y seguidamente visual foro.</p> 
-                   <p  class="eys-parrafo">Hoy en día creso sisemas web combinando HTML, CSS, JavaScrip y PHP; tambien aplicaciones para dispositivo androi para lo cual utilizo el framework android Studio, el cual utiliza el lenguaje de JAVA.</p>
+                   <p  class="eys-parrafo">En la actualidad me encuentro elaborando una serie de proyectos con un primo ex estudiando de la Universidad Tecnica Luis Vargas Torres diseñando sistemas web combinando HTML, CSS, JavaScrip y PHP los cuales aprendimo en lo largo de nuestros estudias y seguimos aprendiendolos para poderlos perfeccionar, nuestro situo web es<b><a href="https://juniortecnologic.com/"> juniortecnologic.</a></b></p>
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo manuales de los leguajes de progración que namejo, y tambien muchos noticias de las ultimas tendencias en tecnología.</p>
+                   <p   class="eys-parrafo">En mis ratos libres practico lo que es diferentes instrumentos como la guitarra electrica , piano , bajo , tambien me gusta mucho la literatura y el dibujar a carboncillo retratos de surrealismo abstracto. </p>
        
 
                    
