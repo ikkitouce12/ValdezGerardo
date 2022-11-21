674c724b048d98a3c01530d6e81dc92af47f1ad3
ikkitouce12 <93734124+ikkitouce12@users.noreply.github.com>
2022-11-20 23:41:28 -0500

    Add files via upload

--- marco-right.php
+++ marco-right.php
@@ -7,15 +7,19 @@
     				<div class="w3-card-4" style="margin: auto;   width:100%; border-top:5px solid green;  border-bottom:5px solid green; padding:0.5em; height:200px;border-radius:50%;">
          				<img id="foto" style="border-radius:50%; width:100%; height:100%;" src="http://educaysoft.org/images/coordinador-MTI.png" alt="El coordinador del programa" >
     				</div>
-<center><span id="elnombre" style="padding:5px; border:1px solid gray; margin:0; background-color: green; color:white; text-shadow: 1px 1px black; ">Kevin Aguilar</span></center>
+<center><span id="elnombre" style="padding:5px; border:1px solid gray; margin:0; background-color: green; color:white; text-shadow: 1px 1px black; ">Valdez Gerardo</span></center>
 <hr>
 			  </div>
 
     </div>
     <br>
     <div id="blog-indice">
-        <p>Indice</p>
-        <a href="#softskilleducacion"> softskill en la educación</a>
+        <p>Contenido</p>
+		<a href='#'>Inicio</a>
+        <a href='#blog-seccion2'>Estudios</a>
+        <a href='#blog-seccion2'>Conocimiento Informático</a>
+        <a href='#blog-seccion2'>Hooby</a>
+    	<a href="donacion.php">&#128151; Donaciones</a>
     </div>
 
 </div>
