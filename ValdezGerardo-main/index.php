3fa81bfb0000df77dc3ef63fc197287b66778461
ikkitouce12 <93734124+ikkitouce12@users.noreply.github.com>
2022-11-20 23:11:31 -0500

    Add files via upload

---
+++ ValdezGerardo-main/index.php
@@ -0,0 +1,93 @@
+
+<?php
+include("head.php");
+?>
+
+
+<?php
+include("marco-right.php");
+?>
+   
+ <div style="float:left; width:60%; margin:60px auto; padding:1vw">
+   <div  style="width: 100%;">     
+    <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
+       <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
+        	<h1>¿Quién es Gerardo Valdez?</h1>
+       		 <hr>
+             
+          <div id="posteo" style="text-align: left; font-size:0.5vw;">
+                Posteado por:  Gerardo Valdez<br>
+                Fecha: 2022-30-10<br>
+          </div>
+        </div>
+    </div>
+  </div>
+  <div style="width: 100%; height:100%;" >
+           <div  style="text-align:justify; font-size: 20px;">
+                   <p class="eys-parrafo"> Hola , yo soy Gerardo Valdez , estudiante de la carrera de Ing. en Tic`s` actuamente me encutro crusando el 7mo año de carrera en la Universidad Tecnica Luis Vargas Torres, en la cual me gusta mucho y me apasiona cada dia mas el tema de la tecnologia .</p>
+                   <p class="eys-parrafo"> Desde que estudie la secundaria me gusto mucho lo que era la informatica y la tecnologia , por la cual quise seguir esta carrera , hace pocos meses comense a incurcionar en lo que era el software libre y me encutro aprendiendo cada dia un poco mas sobre este nuevo
+				   mundo el cual es muy interezante y facinante.</p>
+<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar fue en el colegio el cual era c++ <b></b></p>
+
+   <div id="blog-seccion2"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2> 
+    
+
+
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Particula a Distancia Juan Montalvo donde me gradue en el año 2013, de la especialidad de Informatica, apenas me gradue me incline por mi otra pasion que fue la musica y me decide por irme a estudiar a la Universidad de las Artes ubicada en la ciudad de Guayaquil , pero retorne a la ciudad donde ya en el año 2018 retome mis estudios informaticos Ingresando a la Universidad Luis Vargas Torres en la carrera de Ing. en Tic`s`, donde cruzando el primer año comense a laborar como docente en el Colegio Particular Cristiano del Salvador diviendo el tiempo entre la Universidad y el trabajo donde adquire mucha esperiencia como docente implantiendo 
+				   materias como Dibujo tecnico y artisco, Musica, Ciencias Sociales ,Cultura Fisca y en ocaciones una que otra catedra de programacion a los estudiantes de primero de bachillerato.</p>
+				   <p class="eys-parrafo"></p>
+<p class="eys-parrafo"> </p>
+   <div id="blog-seccion3"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
+    
+
+                   <p  class="eys-parrafo">En la actualidad me encuentro elaborando una serie de proyectos con un primo ex estudiando de la Universidad Tecnica Luis Vargas Torres diseñando sistemas web combinando HTML, CSS, JavaScrip y PHP los cuales aprendimos en lo largo de nuestros estudios y seguimos aprendiendolos para poderlos perfeccionar, nuestro situo web es<b><a href="https://juniortecnologic.com/"> juniortecnologic.</a></b></p>
+   <div id="blog-seccion4"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
+    
+
+                   <p   class="eys-parrafo">En mis ratos libres practico lo que es diferentes instrumentos como la guitarra electrica , piano , bajo , tambien me gusta mucho la literatura y el dibujar a carboncillo retratos de surrealismo abstracto. </p>
+       
+
+                   
+
+               <hr>
+               <div style=" bottom: 0; margin-bottom: 0;   width:100%; display: inline-block;" >
+                  <div style="width: 40%;  float: left;">
+                   <a href="#">&#171;   Previo</a>
+                  </div>
+
+                  <div style="width: 40%; float: right; text-align: right;">
+                   <a href="apendiendo-cpp">Programa para aprender C++ &#187;</a>
+                  </div>
+
+                </div>
+           </div>
+  </div> 
+
+  </div>
+
+
+
+
+<?php
+include("marco-left.php")
+?>
+ 
+<script >  
+
+ document.title="Stalin"; 
+document.getElementById("foto").src="./images/gerardo.png";
+document.getElementById("elnombre").innerHTML="Valdez Gerardo";
+
+ document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
+
+ document.getElementById("blog-relacionados").innerHTML=
+ 
+<?php
+include("foot.php")
+?>
