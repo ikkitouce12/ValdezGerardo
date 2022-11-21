3873eba007430f9fd50486ab6586adfa0c3bd0d6
ikkitouce12 <93734124+ikkitouce12@users.noreply.github.com>
2022-11-20 23:45:30 -0500

    Add files via upload

--- marco-left.php
+++ marco-left.php
@@ -1,7 +1,12 @@
   <div style="float:right; padding: 1vw: 82px; width: 20%;border-left: 1px solid black;">
     <div id="blog-relacionados" style=" width: 100%; height: 100vh;   padding: 1vw; margin-top:80px;"> 
         <p>Temas Relacionados</p>
-        <a href="eys-softskill.php">Softskill</a>
+		<a href='http://highkickesmeraldas.com/RiveraFabricio/'>Fabricio Rivera</a></p><p>
+		<a href='https://highkickesmeraldas.com/GamezRobert/'>Junior Gamez</a></p></p><p>
+		<a href='https://highkickesmeraldas.com/SolorzanoChristopher/'>Christopher Solorzano</a></p><p>
+		<a href='https://highkickesmeraldas.com/CORALalexander/'>Alexander Coral</a></p> <p>
+		<a href='https://highkickesmeraldas.com/ValdezGerardo/'>Gerardo Valdez</a></p> ";
+		
    </div>
   </div>
 
